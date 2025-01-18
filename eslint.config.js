d2a87eb07bd2a78a647d567ddd68a905dc6c1460
Nico Rehwaldt <git_nikku@nixis.de>
2025-01-18 18:11:55 +0100

    chore: lint project

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+
+const files = {
+  build: [
+    'eslint.config.js'
+  ],
+  test: [
+    'test.js'
+  ]
+};
+
+export default [
+
+  // build
+  ...bpmnIoPlugin.configs.node.map(config => {
+
+    return {
+      ...config,
+      files: files.build
+    };
+  }),
+
+  // lib + test
+  ...bpmnIoPlugin.configs.recommended.map(config => {
+
+    return {
+      ...config,
+      ignores: files.build
+    };
+  }),
+
+  // test
+  ...bpmnIoPlugin.configs.mocha.map(config => {
+
+    return {
+      ...config,
+      files: files.test
+    };
+  })
+];
